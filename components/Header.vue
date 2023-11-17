ae06cfa847b4305bd7bb120b6d4f65e263ddd17d
yydaaa <akusayangkamukok24@gmail.com>
2023-11-17 16:11:56 +0700

    beneran benerin ini bener2

---
+++ components/Header.vue
@@ -0,0 +1,203 @@
+<template>
+  <div>
+      <div class="nav-container">
+          <img src="@/assets/images/logosmk.png" class="logosmk"/>
+          <div class="headerText">
+              <h1>PERPUS DIGIAL
+                  SMKN 4 TASIKMALAYA</h1>
+          </div>
+          <h4>KAMBING TUA</h4>
+          <img src="@/assets/images/logoclub.png" class="logoclub"/>
+
+          
+      </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+<style scoped>
+*{
+margin: 0;
+z-index: 1;
+}
+.nav-container {
+  background-color:#FF9900;
+  width: 100%;
+  height: 150px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+
+.logoclub {
+  width: 100px;
+  height: 90px;
+}
+
+.logosmk{
+  width: 111px;
+  height: 126px;
+
+}
+h1 {
+  color: #ffffff;
+  text-align: right;
+  font-family: '';
+  font-size: 20px;
+  letter-spacing: 9px;
+  font-weight: 50;
+  line-height: normal;
+
+}
+
+h4{
+  color: #ffffff;
+  text-align: right;
+  font-size: 50px;
+  letter-spacing: 4px;
+  font-weight: 60;
+}
+
+@media (max-width: 520px) {
+.nav-container {
+    background-color:#FF9900;
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+}
+
+.logoclub {
+    width: 150px;
+    height:100px;
+}
+  
+}
+.logosmk{
+    width: 46.077px;
+    height: 50px;
+
+}
+h1 {
+  color: #ffffff;
+    text-align: right;
+    font-family: '';
+    font-size: 26px;
+    letter-spacing: 4px;
+    font-style: 'bold';
+  
+}
+  
+  h4{
+    color: #ffffff;
+    text-align: center;
+    font-family: 'Charm', cursive;
+    font-size: 35px;
+    letter-spacing: 2px;
+    font-style: 'bold';
+    font-weight: 60;
+}
+
+@media (max-width: 820px) {
+.nav-container {
+    background-color:#FF9900;
+    width: 100%;
+    height: 130px;
+    display: flex;
+      justify-content: space-evenly;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+  
+  .logoclub {
+      width: 100px;
+      height:118px;
+  }
+  
+  
+  .logosmk{
+      width: 80.077px;
+      height: 80px;
+  
+  }
+  h1 {
+      color: #EFDE79;
+      text-align: center;
+      font-family: '';
+      font-size: 15px;
+      letter-spacing: 2px;
+      font-style: 'bold';
+  
+  }
+  
+  h4{
+      color: #fff9c2;
+      text-align: left;
+      font-family: 'Charm', cursive;
+      font-size: 12px;
+      font-weight: 60;
+  }
+  #name {
+    width: 335px;
+    height: 50px;
+    font-size: 20px;
+    padding-left: 15px;
+    background-color: #fff9c2;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border-radius: 50px;
+    border-style: hidden;
+}
+#kategori {
+    width: 335px;
+    height: 50px;
+    font-size: 20px;
+    padding-left: 15px;
+    background-color: #fff9c2;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border-radius: 50px;
+    border-style: hidden;
+
+}
+#keperluan {
+    width: 335px;
+    height: 50px;
+    font-size: 20px;
+    padding-left: 15px;
+    background-color: #fff9c2;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border-radius: 50px;
+    border-style: hidden;
+}
+/* End Visitor Style */
+/* History Style */
+.showData {
+    background-color: #483800ec;
+    display: flex;
+    flex-direction: column;
+    padding: 60px 5px 5px 0px;
+}
+
+.table-responsive{
+    border: 2px solid #A59663;
+    font-family: 'Chau Philomene One', sans-serif;
+    color: wheat;
+    margin: 15px;
+    letter-spacing: 3px;
+
+}
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,127 @@
+<template>
+<!--Input Data For DataBase-->
+<div class="content">
+    <form @submit.prevent="addData" >
+        <div class="row">
+            <div class="col-sm-10">
+                <input v-model="name" type="text" class="form-control form-control-lg" id="name" placeholder="Nama..." required>
+            </div>
+            </div>
+
+        <div class="row">
+            <div class="col-sm-10">
+                <input v-model="kategori" type="text" class="form-control form-control-lg" id="kategori" placeholder="Kategori..." required>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-10">
+                <input v-model="keperluan" type="text" class="form-control form-control-lg" id="keperluan" placeholder="Keperluan..." required>
+            </div>
+        </div>  
+        <button class="btn">SUBMIT</button>
+    </form>
+</div>
+
+<!--Showing Data Tables-->
+<div class="showData">
+    <h1>Riwayat Pengunjung</h1>
+    <div class="table-responsive">
+        <div class="m-2">Menampilkan {{ datas.length }} data</div>
+        <table class="table table-responsive">
+            <thead>
+                <tr>
+                    <th class="no">No</th>
+                    <th class="nama">Nama</th>
+                    <th class="kategori">Kategori</th>
+                    <th class="keperluan">Keperluan</th>
+                    <th class="tanggal">Tanggal</th>
+            </tr>
+        </thead>
+        <tbody v-for="(kunjungan,index) in datas" :key="kunjungan.id ">
+            <td>{{ index + 1 }}</td>
+            <td>{{ kunjungan.nama }}</td>
+            <td>{{ kunjungan.kategori }}</td>
+            <td>{{ kunjungan.keperluan }}</td>
+            <td>{{ kunjungan.tanggal }}</td>
+        </tbody>
+    </table>
+    </div>
+</div>
+</template>
+
+
+<script setup>
+const supabase = useSupabaseClient()
+const datas = ref([])
+const name= ref('')
+const kategori= ref('')
+const keperluan= ref('')
+
+
+async function getData(){
+    const{data,error}=await supabase
+    .from("kunjungan")
+    .select()
+    if (error) throw error
+    if (data){
+    console.log(data)
+    datas.value=data
+}
+}
+// Add Data From Input To Database
+async function addData(){
+    const { error } = await supabase
+    .from('kunjungan')
+    .insert([{
+        kategori: kategori.value,
+        nama: name.value,
+        keperluan: keperluan.value
+    }])
+    if (error) throw error
+    else getData()
+}
+onMounted(() =>{
+    getData()
+})
+</script>
+<style scope>
+.content {
+    background-image: url("@/assets/images/bg.png");
+    width: 100%;
+    height: 700px;
+    padding-top: 150px;
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-content: center;
+}
+
+@keyframes animate {
+    0% {
+        background-position: -500%;
+    }
+    100% {
+        background-position: 1000%;
+    }
+}
+.btn{
+    width: 100px;
+    height: 30px;
+    color: #000000;
+    background-color: #ffd900c9 ;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border-radius: 20px;
+    float: center;
+    margin: 10px 50px ;
+}
+.btn:hover{
+    background-color: #ffd900c9 ;
+    color: #000000;
+}
+form {
+    margin-top: 25px;
+}
+
+</style>
